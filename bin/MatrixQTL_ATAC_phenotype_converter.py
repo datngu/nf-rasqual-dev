e563a36dc3215233c510ce672fc1d57a10d19656
Dat T Nguyen <datn@Dats-MacBook-Pro.local>
2022-11-04 18:19:14 +0100

    eigenMT

--- bin/MatrixQTL_ATAC_phenotype_converter.py
+++ bin/MatrixQTL_ATAC_phenotype_converter.py
@@ -4,7 +4,7 @@ import sys
 import argparse
 
 parser = argparse.ArgumentParser(description = "Convert count file to phenotype data input of MatrixQTL", formatter_class=argparse.ArgumentDefaultsHelpFormatter)
-parser.add_argument("--count", help = "count file input")
+parser.add_argument("--count", help = "ATAC count file input")
 parser.add_argument("--out_phenotype", help = "phenotype expression output")
 parser.add_argument("--out_phenotype_position", help = "phenotype position output")
 
@@ -16,22 +16,23 @@ fout_pos = open(args.out_phenotype_position, 'w')
 
 for l in fi:
     tokens = l.split("\t")
-    if tokens[0] == "gene_id":
+    if tokens[0] == "Geneid":
         tokens[0] = "ID"
-        l1 = "\t".join(tokens[0:1] + tokens[5:])
-        l1 = l1 + "\n"
+        l1 = "\t".join(tokens[0:1] + tokens[6:])
+        #l1 = l1 + "\n"
         fout_phen.writelines(l1)
         l2 =  "\t".join(["gene_id", "chrom_probe", "s1", "s2"])
         l2 = l2 + "\n"
         fout_pos.writelines(l2)
     else:
-        l1 = "\t".join(tokens[0:1] + tokens[5:])
-        l1 = l1 + "\n"
+        gene_id = ":".join(tokens[0:6])
+        l1 = "\t".join([gene_id] + tokens[0:1] + tokens[5:])
+        #l1 = l1 + "\n"
         fout_phen.writelines(l1)
-        gene_id = tokens[0]
+        
         chr = tokens[1]
-        stat = tokens[3].split(",")[0]
-        end = stat = tokens[4].split(",")[-1]
+        stat = tokens[2]
+        end = stat = tokens[3]
         l2 = "\t".join([ gene_id, chr, stat, end])
         l2 = l2 + "\n"
         fout_pos.writelines(l2)
@@ -39,3 +40,5 @@ for l in fi:
 fi.close()
 fout_phen.close()
 fout_pos.close()
+
+# python3 ./bin/MatrixQTL_ATAC_phenotype_converter.py --count data/atac_consensus_peak_featureCounts_filtered.txt --out_phenotype tes1 --out_phenotype_position tes2
---
+++ bin/MatrixQTL_RNA_phenotype_converter.py
@@ -0,0 +1,44 @@
+#!/usr/bin/env python
+
+import sys
+import argparse
+
+parser = argparse.ArgumentParser(description = "Convert count file to phenotype data input of MatrixQTL", formatter_class=argparse.ArgumentDefaultsHelpFormatter)
+parser.add_argument("--count", help = "RNA count file input")
+parser.add_argument("--out_phenotype", help = "phenotype expression output")
+parser.add_argument("--out_phenotype_position", help = "phenotype position output")
+
+args = parser.parse_args()
+
+fi = open(args.count, 'rt')
+fout_phen = open(args.out_phenotype, 'w')
+fout_pos = open(args.out_phenotype_position, 'w')
+
+for l in fi:
+    tokens = l.split("\t")
+    if tokens[0] == "gene_id":
+        tokens[0] = "ID"
+        l1 = "\t".join(tokens[0:1] + tokens[5:])
+        #l1 = l1 + "\n"
+        fout_phen.writelines(l1)
+        l2 =  "\t".join(["gene_id", "chrom_probe", "s1", "s2"])
+        l2 = l2 + "\n"
+        fout_pos.writelines(l2)
+    else:
+        l1 = "\t".join(tokens[0:1] + tokens[5:])
+        #l1 = l1 + "\n"
+        fout_phen.writelines(l1)
+        gene_id = tokens[0]
+        chr = tokens[1]
+        stat = tokens[3].split(",")[0]
+        end = stat = tokens[4].split(",")[-1]
+        l2 = "\t".join([ gene_id, chr, stat, end])
+        l2 = l2 + "\n"
+        fout_pos.writelines(l2)
+
+fi.close()
+fout_phen.close()
+fout_pos.close()
+
+
+# python3 ./bin/MatrixQTL_RNA_phenotype_converter.py --count data/rna_gene_level_count_salmon_filtered.txt --out_phenotype tes1 --out_phenotype_position tes2
